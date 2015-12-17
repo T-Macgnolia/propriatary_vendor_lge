04655102f8381eb1ee391dc32e3b74fbac5b65cc
T-Macgnolia <tmacgnolia@gmail.com>
2015-12-17 06:10:26 -0500

    Added path for com.qualcomm.location.apk

--- g4stylus-common/g4stylus-common-vendor-blobs.mk
+++ g4stylus-common/g4stylus-common-vendor-blobs.mk
@@ -184,6 +184,7 @@ PRODUCT_COPY_FILES += \
     vendor/lge/g4stylus-common/proprietary/lib/libloc_core.so:system/lib/libloc_core.so \
     vendor/lge/g4stylus-common/proprietary/lib/libloc_ds_api.so:system/lib/libloc_ds_api.so \
     vendor/lge/g4stylus-common/proprietary/lib/libloc_eng.so:system/lib/libloc_eng.so \
+    vendor/lge/g4stylus-common/proprietary/priv-app/com.qualcomm.location/com.qualcomm.location.apk:system/priv-app/com.qualcomm.location/com.qualcomm.location.apk \
     vendor/lge/g4stylus-common/proprietary/vendor/bin/slim_ap_daemon:system/vendor/bin/slim_ap_daemon \
     vendor/lge/g4stylus-common/proprietary/vendor/lib/hw/flp.default.so:system/vendor/lib/hw/flp.default.so \
     vendor/lge/g4stylus-common/proprietary/vendor/lib/libgeofence.so:system/vendor/lib/libgeofence.so \
